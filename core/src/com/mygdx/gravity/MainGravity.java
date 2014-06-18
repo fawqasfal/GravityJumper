8087d09261918171448179837246a693e99acfb6
omarhegazy <omarbicboi@gmail.com>
2014-06-18 14:40:22 -0400

    updated for further screen development

--- core/src/com/mygdx/gravity/MainGravity.java
+++ core/src/com/mygdx/gravity/MainGravity.java
@@ -1,27 +1,30 @@
 package com.mygdx.gravity;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 
-public class MainGravity extends ApplicationAdapter {
+
+public class MainGravity extends Game {
 	SpriteBatch batch;
-	Texture img;
+	BitmapFont font;
+	static final int WIDTH = 800;
+	static final int HEIGHT = 800;
 	
-	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("emulogic.ttf"));
+		font = generator.generateFont(14);
+		this.setScreen(new MainMenuScreen(this));
+	}
+	public void render() {
+		super.render();
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void dispose() {
+	    batch.dispose();
+	    font.dispose();
 	}
 }
---
+++ core/src/com/mygdx/gravity/ScreenAdapter.java
@@ -0,0 +1,12 @@
+package com.mygdx.gravity;
+import com.badlogic.gdx.Screen;
+
+public class ScreenAdapter implements Screen {
+	public void render(float delta) {}
+	public void hide() {}
+	public void pause() {}
+	public void resume() {}
+	public void resize(int width, int height) {}
+	public void dispose() {}
+	public void show() {}
+}
