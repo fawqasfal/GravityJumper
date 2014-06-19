068aac8bf125dd2b5c4567f8c5706f789d2b946f
omarhegazy <omarbicboi@gmail.com>
2014-06-19 00:29:16 -0400

    platform stuff

--- core/src/com/mygdx/gravity/MainGameScreen.java
+++ core/src/com/mygdx/gravity/MainGameScreen.java
@@ -12,25 +12,35 @@ import com.badlogic.gdx.utils.*;
 public class MainGameScreen extends ScreenAdapter {
 	MainGravity game;
 	Texture bgImage;
+	Texture platform;
+	Hero hero;
+	Music gameMusic;
 	OrthographicCamera camera;
+	Array<Platform> platforms; 
 	int w = MainGravity.WIDTH;
 	int h = MainGravity.HEIGHT;
 
 	
 	public MainGameScreen(MainGravity game) {
 		this.game = game;
-
 		//images
 		bgImage = new Texture(Gdx.files.internal("background.png"));
+
 		//sound
+		gameMusic = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
 		//play in show
 
    		camera = new OrthographicCamera();
    		camera.setToOrtho(false, w, h);
 		
-		//hero
-
+		
 		//platforms
+		platforms = new Array<Platform>();
+		platforms.add(new Platform(0,0));
+		platforms.add(new Platform(64 * Platform.SCALE, 0));
+		platforms.add(new Platform(256 * Platform.SCALE, 0));
+		platforms.add(new Platform(0, 30 * Platform.SCALE));
+		platforms.add(new Platform(0, 60 * Platform.SCALE));
 	}
 
 	public float randrange(float low, float high) {
@@ -44,7 +54,13 @@ public class MainGameScreen extends ScreenAdapter {
 		game.batch.setProjectionMatrix(camera.combined);
  		game.batch.begin();
  			game.batch.draw(bgImage,0,0);
+ 			for (Platform platform : platforms) {
+ 				game.batch.draw(platform.getImage(), platform.getRect().x, platform.getRect().y, 
+ 					platform.getRect().width, platform.getRect().height);
+ 			}
    		game.batch.end();
+
+
    		camera.update();
 	}
 
@@ -53,5 +69,10 @@ public class MainGameScreen extends ScreenAdapter {
 	     game.batch.dispose();
 	}
 	
+	public void show() {
+	    // start the playback of the background music
+	    // when the screen is shown
+	    //gameMusic.play(); -- commented out because im listening to led zeppelin now
+	}
 
 }
--- core/src/com/mygdx/gravity/Platform.java
+++ core/src/com/mygdx/gravity/Platform.java
@@ -9,15 +9,27 @@ public class Platform {
 	TextureRegion image;
 	Rectangle rectRep;
 
-	public static final int DEFAULT_IMAGE_HEIGHT = 15;
+	public static final int DEFAULT_IMAGE_HEIGHT = 5;
 	public static final String DEFAULT_IMAGE = "platform.png";
-
-	public Platform(Texture image, float startX, float startY, float imgX, float imgY, int width, int height) {
-		this.image = new TextureRegion(image, imgX, imgY, width, height);
-		this.rectRep = new Rectangle(startX, startY, width, height);
+	public static final float SCALE = 5f;
+	public Platform(Texture image, float startX, float startY, int width, int height) {
+		this.image = new TextureRegion(image, 0, width - height, width, height);
+		/*why width - height? Assuming default image is a power-of-two, this is imgHeight - actualHeightOfObject.
+		actualHeightOfObject is different from imgHeight because the platform is not a power of two, meaning there is 
+		going to be some blank space at the top that we're going to get rid of. */
+		this.rectRep = new Rectangle(startX, startY, width * SCALE, height * SCALE);
 	}
 	public Platform(float spawnX, float spawnY) {
-		Texture defImage = new Texture(Gdx.files.internal(DEFAULT_IMAGE));
-		new Platform(defImage, spawnX, spawnY, 0, 0, 64, DEFAULT_IMAGE_HEIGHT);
+		this(new Texture(Gdx.files.internal(DEFAULT_IMAGE)), spawnX, spawnY, 64, DEFAULT_IMAGE_HEIGHT);
 	}
+
+	public TextureRegion getImage() {
+		return this.image;
+	}
+
+	public Rectangle getRect() {
+		return this.rectRep;
+	}
+
+
 }
