30146af559d8a9480da0c0024db12bd0ff696361
Shein Htike <chocolateshein.htike@gmail.com>
2014-06-18 23:15:25 -0400

    fixed platform

--- core/src/com/mygdx/gravity/Platform.java
+++ core/src/com/mygdx/gravity/Platform.java
@@ -1,5 +1,6 @@
 package com.mygdx.gravity;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
@@ -9,12 +10,14 @@ public class Platform {
 	Rectangle rectRep;
 
 	public static final int DEFAULT_IMAGE_HEIGHT = 15;
+	public static final String DEFAULT_IMAGE = "platform.png";
 
-	public Platform(Texture image, float startX, float startY, int width, int height) {
-		this.image = new TextureRegion(image, startX, startY, width, height);
+	public Platform(Texture image, float startX, float startY, float imgX, float imgY, int width, int height) {
+		this.image = new TextureRegion(image, imgX, imgY, width, height);
 		this.rectRep = new Rectangle(startX, startY, width, height);
 	}
-	public Platform(Texture image) {
-		new Platform(image, 0, 0, 64, DEFAULT_IMAGE_HEIGHT);
+	public Platform(float spawnX, float spawnY) {
+		Texture defImage = new Texture(Gdx.files.internal(DEFAULT_IMAGE));
+		new Platform(defImage, spawnX, spawnY, 0, 0, 64, DEFAULT_IMAGE_HEIGHT);
 	}
 }
