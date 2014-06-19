c63858168143e5bb91f22f85f1eb2829d530f9b8
omarhegazy <omarbicboi@gmail.com>
2014-06-19 00:50:41 -0400

    finished platforms stuff

--- core/src/com/mygdx/gravity/Platform.java
+++ core/src/com/mygdx/gravity/Platform.java
@@ -14,7 +14,11 @@ public class Platform {
 	public static final String DEFAULT_IMAGE = "platform.png";
 	public static final float SCALE = 5f;
 	public Platform(Texture image, float startX, float startY, int width, int height) {
-		this.image = new TextureRegion(image, 0, 64 - height, width, height); //given platform image is 64x64
+		this.image = new TextureRegion(image, 0, 64 - height, width, height); 
+		/*given platform image is 64x64. you may ask why i didnt make it 16x16 or at least 32x32, this is because
+		  originally the platform was much more intricate but it was also really ugly because it turns out I don't have
+		  the artistic skills to be that indulgent. then i was too lazy to re-open the pixel art editor and remake the image
+		  in smaller form.*/ 
 		this.rectRep = new Rectangle(startX, startY, width * SCALE, height * SCALE);
 	}
 	public Platform(float spawnX, float spawnY) {
