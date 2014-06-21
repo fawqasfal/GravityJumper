e8c32e90a2d8f7f8ef938d70327df3167f319da6
Shein Htike <chocolateshein.htike@gmail.com>
2014-06-20 23:20:27 -0400

    final commit

--- core/src/com/mygdx/gravity/Enemy.java
+++ core/src/com/mygdx/gravity/Enemy.java
@@ -1,5 +1,5 @@
 package com.mygdx.gravity;
-
+import java.util.*;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.Texture;
@@ -11,17 +11,33 @@ public class Enemy {
 	TextureRegion defImage;
 	int damage;
 	int give;
+	float velocity;
+	float acceleration = 50f;
+	float slowdown = 0.000002f; 
+	//how long have we been moving in either left/right or up/down. 
+	//every time move in the same direction, add 1 (up/right), or -1 (left/down). 
+	//every time change, set to 1 (up/right) or -1 (left/down)
+
 	boolean alive; 
 
 	public final static String DEFAULT_IMAGE = "enemy.png";
 	public final static float DEFAULT_SIZE = 16;
-	public final static float SCALE = 4.5f;
-	public final static int MOVE_AMT = 100;
+	public final static int MIN_DMG = 5;
+	public final static int MAX_DMG = 10;
+	public final static int MIN_GIVE = 50;
+	public final static int MAX_GIVE = 100;
+	public final static float SCALE = 3.5f;
+	public final static float MOVE_AMT_X = 3f;
+	public final static float MOVE_AMT_Y = 2f;
+	public final static int MAX_X = 0;
+	public final static int MAX_Y = 0;
+
 	public Enemy(float spawnX, float spawnY, int dmg, int give) {
 		//It takes a nation of millions to hold us back.
 		this.rectRep = new Rectangle(spawnX, spawnY, SCALE * DEFAULT_SIZE, SCALE * DEFAULT_SIZE);
 		this.defImage = new TextureRegion(new Texture(Gdx.files.internal(DEFAULT_IMAGE)));
-		this.damage = damage;
+		this.damage = dmg;
+		
 		this.give = give;
 		this.alive = true;
 	}
@@ -32,24 +48,27 @@ public class Enemy {
 		this.defImage = null;
 	}
 
-	public void floatLeft(int amt) {
-		this.rectRep.x -= amt;
-		floatY(amt);	
+	public void move(float amtX, float amtY, Hero hero) {
+		//will angrily shake while trying to reach hero
+		this.rectRep.x += amtX;
+		this.rectRep.y += amtY;
+		this.velocity += this.acceleration;
+		this.rectRep.x += slowdown * Math.random() * velocity * (hero.rectRep.x - this.rectRep.x);
+		this.rectRep.y += slowdown * Math.random() * velocity * (hero.rectRep.y - this.rectRep.y);
+
 	}
 
-	public void floatRight(int amt) {
-		this.rectRep.x += amt;
-		floatY(amt);
-	}
+	public boolean collide(Platform platform) {
+		if (this.collide(platform.rectRep)) {
 
-	public void floatY(int amt) {
-		int direction = randrange(-1,2);
-		this.rectRep.y += direction * Math.log(amt) / Math.log(2);
+			return true;
+		} else{
+			return false;
+		}
 	}
-
-	public int randrange(int low, int high) {
-		//[low, high)
-		return low + (int) Math.floor(Math.random() * (high - low));
-	}
-	
+	public boolean collide(Rectangle object) {
+		boolean intersects = (this.rectRep.x <= object.x + object.width && this.rectRep.x + this.rectRep.width >= object.x && 
+					  this.rectRep.y <= object.y + object.height && this.rectRep.y + this.rectRep.height >= object.y);
+		return intersects;
+	}	
 }
